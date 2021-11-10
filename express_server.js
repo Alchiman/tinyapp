9c9504a791a98e2c2c2fb459c4f825e0b6e83527
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-10 15:52:16 -0800

    added the login form and the logout button

--- express_server.js
+++ express_server.js
@@ -3,21 +3,21 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
-app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
+app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
+// this is my randomizer function
 function generateRandomString() {
   return Math.random()
     .toString(15)
     .substring(4, 9);
 }
-
+// this is my home endpoint
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -26,37 +26,42 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
-});
+// app.get("/hello", (req, res) => {
+//   const templateVars = { greeting: "Hello World!" };
+//   res.render("hello_world", templateVars);
+// });
 
+// here is the endpoint for list of existing urls
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["userName"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// endpoint for creating new urls
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["userName"] };
+  res.render("urls_new", templateVars);
 });
 
-// this endpoint handles new long urls
+// this endpoint handles posts for new
+
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  console.log(req.body);
+  res.send("Ok");
 });
 
 // this endpoint handles /login and set cookie
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  // res.send(username);
-  res.cookie("cookieName", username);
-  console.log("Cookies: ", req.cookies);
-  // console.log(username);
+  res.cookie("userName", username);
+  res.redirect("/urls");
 });
-// this endpoint handles /login and set cookie2
-// app.get("/login", (req, res) => {});
 
+// endpoint for logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("userName");
+  res.redirect("/urls");
+});
 //updates the URL resource
 
 app.post("/urls/:id", (req, res) => {
@@ -88,6 +93,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL],
+    username: req.cookies["username"],
   };
 
   res.render("urls_show", templateVars);
